1dfa84020bbf8b91560a84605d9d470f6bf1cc4b
Dziugas Vysniauskas <dziugas@Dziugass-MacBook-Pro.local>
2017-09-27 18:23:12 +0300

    Bullet lifetime and camera positioning

--- Assets/Our/Scripts/Bullet.cs
+++ Assets/Our/Scripts/Bullet.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+	public float Lifetime = 3.0f;
+
+	void Start(){
+		Destroy(gameObject, Lifetime);
+	}
+
 	void OnCollisionEnter2D(Collision2D collision){
 		Debug.Log ("Guten tag i'm collision");
 		var health = collision.collider.GetComponent<Health>();
